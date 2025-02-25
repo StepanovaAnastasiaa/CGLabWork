1d27a99dfaf85b2ce06e0aa4c75e922a761572a4
Stepanova <zakazakaxy@gmail.com>
2025-02-25 14:41:09 +0400

    Домашние доделывания

--- lab1/rendering.py
+++ lab1/rendering.py
@@ -15,7 +15,7 @@ def draw_line(img_mat, x0, y0, x1, y1, color):
         y0, y1 = y1, y0
 
     y = y0
-    dy = 2*(x1 - x0)*abs(y1 - y0) / (x1 - x0)
+    dy = 2*abs(y1 - y0)
     derror = 0
     y_update = 1 if y1 > y0 else -1
 
@@ -35,30 +35,33 @@ img = np.zeros((1000, 1000,3), dtype=np.uint8)
 img[0:1000,0:1000,1]=120
 
 v=[]
-file=open('C:\\Users\\Admin\\Desktop\\model_1.obj')
-for str in file:
-    splitted_str=str.split()
-    if (splitted_str[0]=='v'):
-        v.append([float(splitted_str[1]), float(splitted_str[2]), float(splitted_str[3])])
+with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
+    for str in file:
+        splitted_str=str.split()
+        if (splitted_str[0]=='v'):
+            v.append([float(splitted_str[1]), float(splitted_str[2]), float(splitted_str[3])])
 
 for vertex in v:
     img[int(5000*vertex[0]+500),int(5000*vertex[1]+500)]=[255, 255, 255]
 
 f=[]
-for str2 in file:
-    splitted_str2=re.split(r'[ /]', str2)
+with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
+    for str2 in file:
+        splitted_str2=re.split(r'[ /]', str2)
 
-    if (splitted_str2[0]=='f'):
-        f.append([int(splitted_str2[1]), int(splitted_str2[2]), int(splitted_str2[3])])
-# for i in len(f):
-#     x0=v[f[i][0] - 1][0]
-#     y0 = v[f[i][0] - 1][1]
-#
-#     x1=v[f[i][1] - 1][0]
-#     y1 = v[f[i][1] - 1][1]
+        if (splitted_str2[0]=='f'):
+            f.append([int(splitted_str2[1]), int(splitted_str2[4]), int(splitted_str2[7])])
+for face in f:
+    x0=v[face[0] - 1][0]
+    y0 = v[face[0] - 1][1]
 
-
+    x1=v[face[1] - 1][0]
+    y1 = v[face[1] - 1][1]
+    draw_line(img, 5000*y0+500, 5000*x0+500, 5000*y1+500, 5000*x1+500, [255,255,255])
+    x2=v[face[2]-1][0]
+    y2=v[face[2]-1][1]
+    draw_line(img, 5000 * y0 + 500, 5000 * x0 + 500, 5000 * y2 + 500, 5000 * x2 + 500, [255, 255, 255])
+    draw_line(img, 5000 * y1 + 500, 5000 * x1 + 500, 5000 * y2 + 500, 5000 * x2 + 500, [255, 255, 255])
 img = Image.fromarray(img, mode='RGB')
-
-img.save('image.jpg')
-
+im_rotate = img.rotate(90)
+im_rotate.save('image.jpg')
