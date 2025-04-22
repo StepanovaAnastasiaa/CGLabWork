a08166e9daff484fb8e47491167218f9590f9c64
Stepanova <zakazakaxy@gmail.com>
2025-04-22 15:53:44 +0400

    4 лаба

--- lab/rendering.py
+++ lab/rendering.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image
 import math as math
-import re
 
 w=8000
 h=8000
@@ -44,7 +43,7 @@ def bar_coor(x0, y0, x1, y1, x2, y2, x, y):
     l2 = 1-l0-l1
     return[l0, l1, l2]
 
-def draw_triangle(img_mat, z_buffer, x0, y0, x1, y1, x2, y2, z0, z1, z2, i0, i1, i2):
+def draw_triangle(img_mat, z_buffer, x0, y0, z0, u0, v0, x1, y1,  z1, u1, v1, x2, y2, z2, u2, v2, i0, i1, i2, texture):
     xmin = math.floor(min(x0, x1, x2))
     if xmin < 0:
         xmin = 0
@@ -67,8 +66,18 @@ def draw_triangle(img_mat, z_buffer, x0, y0, x1, y1, x2, y2, z0, z1, z2, i0, i1,
 
                 # Проверка на z-буфер
                 if z_hat < z_buffer[i, j]:
-                    color_value = int(-255 * (lambdas[0]*i0+lambdas[1]*i1+lambdas[2]*i2))
-                    color = (max(0,min(color_value, 255)), 255, 255)
+                    # Интерполяция текстурных координат
+                    u = lambdas[0] * u0 + lambdas[1] * u1 + lambdas[2] * u2
+                    v = lambdas[0] * v0 + lambdas[1] * v1 + lambdas[2] * v2
+
+                    # Получение цвета из текстуры (с проверкой границ)
+                    tex_x = min(max(0, int(u * (texture.width - 1))), texture.width - 1)
+                    tex_y = min(max(0, int((1-v) * (texture.height - 1))), texture.height - 1)
+                    tex_color = texture.getpixel((tex_x, tex_y))
+
+                    color_value = int( -255*(lambdas[0]*i0+lambdas[1]*i1+lambdas[2]*i2))
+
+                    color = (max(0,min((tex_color[0]/255)*color_value, 255)), max(0,min((tex_color[1]/255)*color_value, 255)), max(0,min((tex_color[2]/255)*color_value, 255)))
                     img_mat[i, j] = color
                     z_buffer[i, j] = z_hat
 
@@ -114,81 +123,103 @@ img = np.zeros((h, w, 3), dtype=np.uint8)
 img[0:w,0:h,1]=120
 z_buffer = np.full((w, h), np.inf)  # Инициализация z-буфера
 
-ver=[]
-with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
-    for str in file:
-        splitted_str=str.split()
-        if (splitted_str[0]=='v'):
-            ver.append([float(splitted_str[1]), float(splitted_str[2]), float(splitted_str[3])])
+texture = Image.open('C:\\Users\\Admin\\Desktop\\bunny-atlas.jpg').convert('RGB')
+def draw_model(img, z_buffer, texture, w, h, m, alpha, beta, gamma, tx, ty, tz):
+    ver=[]
+    with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
+        for str in file:
+            splitted_str=str.split()
+            if (splitted_str[0]=='v'):
+                ver.append([float(splitted_str[1]), float(splitted_str[2]), float(splitted_str[3])])
 
-v = rot_trans(ver, 0, 180, 90, 0.05, 0.011, 1)
-m=7000
-u0=w/2
-v0=h/2
-for vertex in v:
-    img[int((m*vertex[0])/vertex[2]+u0),int((m*vertex[1])/vertex[2]+v0)]=[255, 255, 255]
+    v = rot_trans(ver, alpha, beta, gamma, tx, ty, tz)
+    #m=7000
+    u0=w/2
+    v0=h/2
+    for vertex in v:
+        img[int((m*vertex[0])/vertex[2]+u0),int((m*vertex[1])/vertex[2]+v0)]=[255, 255, 255]
 
-f=[]
-with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
-    for str2 in file:
-        splitted_str2=str2.split()
-        if (splitted_str2[0] == 'f'):
-            res = []
-            for sp in splitted_str2:
-                s = sp.split('/')
-                if s[0].isdigit(): res.append(int(s[0]))
-            f.append(res)
-light_direction = np.array([0, 0, 1])
-light_magnitude = np.linalg.norm(light_direction)
+    f=[]
+    vt = []
+    vt_num = []
+    with open('C:\\Users\\Admin\\Desktop\\model_1.obj') as file:
+        for str2 in file:
+            splitted_str2=str2.split()
+            if (splitted_str2[0] == 'f'):
+                res_f = []
+                res_vt = []
+                for sp in splitted_str2[1:]:
+                    s = sp.split('/')
+                    res_f.append(int(s[0])-1)
+                    res_vt.append(int(s[1])-1)
 
-f_n = [[0] * 2 for i in range(len(f))]
-for f1 in f:
-    x0 = v[f1[0] - 1][0]
-    y0 = v[f1[0] - 1][1]
-    z0 = v[f1[0] - 1][2]
+                f.append(res_f)
+                vt_num.append(res_vt)
+            if (splitted_str2[0] == 'vt'):
+                vt.append([float(splitted_str2[1]), float(splitted_str2[2])])
 
-    x1 = v[f1[1] - 1][0]
-    y1 = v[f1[1] - 1][1]
-    z1 = v[f1[1] - 1][2]
-    x2 = v[f1[2] - 1][0]
-    y2 = v[f1[2] - 1][1]
-    z2 = v[f1[2] - 1][2]
-    normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
-    f_n[f1[0]-1][1]+= normal
-    f_n[f1[1]-1][1] += normal
-    f_n[f1[2]-1][1] += normal
-    f_n[f1[0] - 1][0] += 1
-    f_n[f1[1] - 1][0] += 1
-    f_n[f1[2] - 1][0] += 1
-for i in range(len(f_n)):
-    if f_n[i][0]!=0:
-        f_n[i][1]/= f_n[i][0]
-for face in f:
-    x0=v[face[0] - 1][0]
-    y0 = v[face[0] - 1][1]
-    z0 = v[face[0] - 1][2]
 
-    x1=v[face[1] - 1][0]
-    y1 = v[face[1] - 1][1]
-    z1 = v[face[1] - 1][2]
-    x2=v[face[2]-1][0]
-    y2=v[face[2]-1][1]
-    z2 = v[face[2] - 1][2]
-    normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+    light_direction = np.array([0, 0, 1])
+    light_magnitude = np.linalg.norm(light_direction)
 
-    i0 = np.dot(f_n[face[0]-1][1], light_direction)/ (np.linalg.norm(f_n[face[0]-1][1]) * np.linalg.norm(light_direction))
-    i1 = np.dot(f_n[face[1] - 1][1], light_direction) / (np.linalg.norm(f_n[face[1] - 1][1]) * np.linalg.norm(light_direction))
-    i2 = np.dot(f_n[face[2] - 1][1], light_direction) / ( np.linalg.norm(f_n[face[2] - 1][1]) * np.linalg.norm(light_direction))
- # Вычисляем косинус угла падения
-    cos_angle = np.dot(normal, light_direction)
-    normal_magnitude = np.linalg.norm(normal)
+    f_n = [[0] * 2 for i in range(len(f))]
+    for f1 in f:
+        x0 = v[f1[0]][0]
+        y0 = v[f1[0]][1]
+        z0 = v[f1[0]][2]
 
-    # Отрисовываем треугольник только если cos_angle < 0
-    if cos_angle / (normal_magnitude * light_magnitude) < 0:
-        # color_value = int(-255 * (cos_angle / (normal_magnitude * light_magnitude)))
-        # color = (color_value, 255, 255)
+        x1 = v[f1[1]][0]
+        y1 = v[f1[1]][1]
+        z1 = v[f1[1]][2]
+        x2 = v[f1[2]][0]
+        y2 = v[f1[2]][1]
+        z2 = v[f1[2]][2]
+        normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+        normal = normal/np.linalg.norm(normal)
+        f_n[f1[0]][1]+= normal
+        f_n[f1[1]][1] += normal
+        f_n[f1[2]][1] += normal
+        f_n[f1[0]][0] += 1
+        f_n[f1[1]][0] += 1
+        f_n[f1[2]][0] += 1
+    for i in range(len(f_n)):
+        if f_n[i][0]!=0:
+            f_n[i][1]/= f_n[i][0]
+    for face, vn in zip(f, vt_num):
 
-        draw_triangle(img, z_buffer, m*x0/z0+u0,m*y0/z0+v0, m*x1/z1+u0, m*y1/z1+v0, m*x2/z2+u0, m*y2/z2+v0, z0, z1, z2, i0, i1, i2)
-img = Image.fromarray(img, mode='RGB')
+        x0=v[face[0]][0]
+        y0 = v[face[0]][1]
+        z0 = v[face[0]][2]
+        tu0 = vt[vn[0]][0]
+        tv0 = vt[vn[0]][1]
 
-img.save('image.jpg')
+        x1=v[face[1]][0]
+        y1 = v[face[1]][1]
+        z1 = v[face[1]][2]
+        tu1 = vt[vn[1]][0]
+        tv1 = vt[vn[1]][1]
+
+        x2=v[face[2]][0]
+        y2=v[face[2]][1]
+        z2 = v[face[2]][2]
+        tu2 = vt[vn[2]][0]
+        tv2 = vt[vn[2]][1]
+        normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+        normal = normal / np.linalg.norm(normal)
+        i0 = np.dot(f_n[face[0]][1], light_direction)/ (np.linalg.norm(f_n[face[0]][1]) * np.linalg.norm(light_direction))
+        i1 = np.dot(f_n[face[1]][1], light_direction) / (np.linalg.norm(f_n[face[1]][1]) * np.linalg.norm(light_direction))
+        i2 = np.dot(f_n[face[2]][1], light_direction) / ( np.linalg.norm(f_n[face[2]][1]) * np.linalg.norm(light_direction))
+    # Вычисляем косинус угла падения
+        cos_angle = np.dot(normal, light_direction)
+        normal_magnitude = np.linalg.norm(normal)
+
+        # Отрисовываем треугольник только если cos_angle < 0
+        if cos_angle / (normal_magnitude * light_magnitude) < 0:
+
+            draw_triangle(img, z_buffer, m*x0/z0+u0,m*y0/z0+v0, z0, tu0, tv0, m*x1/z1+u0, m*y1/z1+v0, z1, tu1, tv1, m*x2/z2+u0, m*y2/z2+v0, z2, tu2, tv2, i0, i1, i2, texture)
+    img = Image.fromarray(img, mode='RGB')
+
+    img.save('image.jpg')
+
+draw_model(img, z_buffer, texture, w, h, 7000, 0, 180, 90, 0.05, 0.011, 1)
+#draw_model(img, z_buffer, texture, w, h, 7000, 0, 90, 120, 0.08, 0.015, 2)
